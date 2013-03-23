713ea7160c8b3fd059c52326d2a8b8199f7912f9
Oana Sandu <ivalyn@gmail.com>
2013-03-23 12:52:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamify::Application.config.secret_token = '0bde442bc17c38866adc5b63c77455b4d2c54bc4c06b3309f15a31f2061551151b7363a11a6804195e84a1253d4287d126f5fdd5bd0ce8c44d3c922d6e81312a'
